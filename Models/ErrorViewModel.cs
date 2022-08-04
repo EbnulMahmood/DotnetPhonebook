7eb4263e7504944e6d8bfed9cd77f0391de6638b
EbnulMahmood <shovonmahmood67@gmail.com>
2022-08-04 18:02:54 +0600

    initialize project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotnetPhonebook.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
